31bb6f16b6b935c2e78614f5181ca7e91367c3c9
Edsel Caprice <caprice@mail.usf.edu>
2018-06-19 23:30:54 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
 
+def my_collect(array)
+i = 0
+new_array = []
+if array.size != 0
+while i < array.length
+  yield new_array << array[i]
+  i += 1
+end
+else 
+new_array << array
+end
+new_array
+end
